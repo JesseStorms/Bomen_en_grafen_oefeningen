6dcc4659cdaefb7c190152b5eba19ed1860104ca
JesseStorms <jesse.storms@student.ucll.be>
2021-02-26 15:36:36 +0100

    Auto stash before merge of "master" and "TomEversdijk/master"

--- week03_BST/src/domain/BinarySearchTree.java
+++ week03_BST/src/domain/BinarySearchTree.java
@@ -11,13 +11,44 @@ public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
 	}
 
 	public boolean lookup(E data) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		if(this.data.equals(data)){
+			return true;
+		}
+		if(this.data == null){
+			return false;
+		}
+		if(this.data.compareTo(data)>0){
+			return this.leftTree.lookup(data);
+		}
+		if(this.data.compareTo(data)<0){
+			return this.rightTree.lookup(data);
+		}
+		return false;
 	}
 
+	@Override
 	public boolean addNode(E data) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		if (this.data.compareTo(data) == 0) {
+			return false;
+		} else {
+			if (data.compareTo(this.data) < 0) {
+				if (this.leftTree == null) {
+					this.leftTree = new BinarySearchTree<E>(data);
+					return true;
+				} else {
+					return this.leftTree.addNode(data);
+				}
+			}
+			else {
+				if (this.rightTree == null) {
+					this.rightTree = new BinarySearchTree<E>(data);
+					return true;
+				} else {
+					return this.rightTree.addNode(data);
+				}
+			}
+		}
 	}
-
 	public boolean removeNode(E data){
 		throw new UnsupportedOperationException("Not yet implemented");
 	}
